6cfb35ebc52a25500cb2df63083f77dbc4d7b29e
Andrew Tomaka <atomaka@gmail.com>
2022-11-01 09:39:45 -0400

    Cleanup/Removal from new UI changes

--- ATMe/ATMe.lua
+++ ATMe/ATMe.lua
@@ -18,7 +18,7 @@ ATMe.taunts =  {
   [185245] = true, -- Torment
   [6795] = true,   -- Growl
   [115546] = true, -- Provke
-  [62124] = true, -- Hand of Reckoning
+  [62124] = true,  -- Hand of Reckoning
   [355] = true,    -- Taunt
 
   [1161] = true,   -- Challenging Shout
@@ -32,9 +32,6 @@ inspectInitialized = false
 local InspectFontStrings = {}
 
 function ATMe.SlashHandler(cmd)
-  if cmd == "frames" then
-    ATMe.moveFrames()
-  end
 end
 
 function ATMe.OnEvent(self, event, ...)
@@ -57,7 +54,7 @@ end
 function events:INSPECT_READY(guid)
   if not inspectInitialized and InspectFrame then
     InspectFontStrings["itemLevel"] = InspectFrame:CreateFontString(nil, "OVERLAY")
-    InspectFontStrings["itemLevel"]:SetPoint("BOTTOMRIGHT", -5, 5)
+    InspectFontStrings["itemLevel"]:SetPoint("BOTTOMLEFT", 10, 5)
     InspectFontStrings["itemLevel"]:SetFont("Fonts\\FRIZQT__.ttf", 24, "OUTLINE")
     InspectFontStrings["itemLevel"]:SetTextColor(1, 1, 1)
 
@@ -102,27 +99,6 @@ end
 function events:PLAYER_LOGIN(...)
   -- found from some forums
   local b=ActionButton8 _MH=_MH or(b:SetAttribute("*type5","macro")or SecureHandlerWrapScript(b,"PreClick",b,'Z=IsAltKeyDown()and 0 or(Z or 0)%8+1 self:SetAttribute("macrotext5","/wm [nomod]"..Z)'))or 1
-
-  QuickJoinToastButton:Hide()
-  ChatFrameChannelButton:Hide()
-  ChatFrameMenuButton:Hide()
-  ChatFrame1ButtonFrame:Hide()
-
-  -- ChatFrame1.ScrollBar:Hide()
-  -- ChatFrame1.ScrollToBottomButton:Hide()
-
-  ChatFrame1:SetWidth(400)
-  ChatFrame1:SetHeight(150)
-
-  ChatFrame1:ClearAllPoints()
-  ChatFrame1:SetClampRectInsets(0, 0, 0, 0)
-  ChatFrame1:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", 0, 0)
-
-  ChatFrame1EditBox:ClearAllPoints()
-  ChatFrame1EditBox:SetPoint("BOTTOMLEFT", ChatFrame1, "TOPLEFT", 0, 0)
-  ChatFrame1EditBox:SetPoint("RIGHT", ChatFrame1, 0, 0)
-
-  ChatFrame1:SetUserPlaced(true)
 end
 
 function events:PLAYER_REGEN_DISABLED(...)
@@ -146,18 +122,6 @@ end
 function events:PLAYER_UPDATE_RESTING(...)
   -- Notify that we should repair
   if ATMe.needsRepair() then StaticPopup_Show("REPAIR_ALERT") end
-end
-
-function ATMe.moveFrames()
-  print("Moving frames to ATMe default locations")
-  PlayerFrame:ClearAllPoints()
-  PlayerFrame:SetPoint("CENTER",UIParent,-350,-225)PlayerFrame:SetUserPlaced(true)
-
-  TargetFrame:ClearAllPoints()
-  TargetFrame:SetPoint("CENTER",UIParent,350,-225)TargetFrame:SetUserPlaced(true)
-
-  FocusFrame:ClearAllPoints()
-  FocusFrame:SetPoint("CENTER",UIParent,-350,150)TargetFrame:SetUserPlaced(true)
 end
 
 function ATMe.announceKey()
@@ -241,10 +205,6 @@ for event, method in pairs(events) do
 end
 f:SetScript("OnEvent", ATMe.OnEvent)
 
---[[ hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
-  TalkingHeadFrame:Hide()
-end) ]]--
-
 function PaperDollFrame_SetMovementSpeed(statFrame, unit)
 	statFrame.wasSwimming = nil
 	statFrame.unit = unit
@@ -263,13 +223,5 @@ CharacterStatsPane.statsFramePool.resetterFunc =
 		FramePool_HideAndClearAnchors(pool, frame)
 	end
 table.insert(PAPERDOLL_STATCATEGORIES[1].stats, { stat = "MOVESPEED"})
-
-for i = 1, 7 do
-  local chat = _G["ChatFrame"..i]
-  local font, size = chat:GetFont()
-  chat:SetFont(font, size, "THINOUTLINE")
-  chat:SetShadowOffset(0, 0)
-  chat:SetShadowColor(0, 0, 0, 0)
-end
 
 SlashCmdList["ATME"] = ATMe.SlashHandler
